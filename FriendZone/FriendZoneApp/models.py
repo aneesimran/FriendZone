f08da70b721a2f3f7c5fdf0d140018736f90480c
Anees Imran <aneesimran@outlook.com>
2018-11-20 18:22:27 +0000

    First meeting

--- FriendZone/FriendZoneApp/models.py
+++ FriendZone/FriendZoneApp/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
-Mijan was here
+class UserProfile(models.Model):
+    userName = models.CharField(max_length = 150)
+    password = models.CharField(max_length = 30)
+    userProfilePic = models.ImageField()
+    email = models.EmailField()
+
+class Profile(models.Model):
+    bio = models.CharField(max_length = 2000)
+    image = models.ImageField(upload_to = 'profile_image')
