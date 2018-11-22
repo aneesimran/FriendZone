1d76f79b3c5ce22ff5487c79f2eff8ff12d16bbe
Anees Imran <aneesimran@outlook.com>
2018-11-22 13:42:29 +0000

    meeting2

--- FriendZone/FriendZoneApp/views.py
+++ FriendZone/FriendZoneApp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {"pageTitle": "Welcome to FriendZone "}
+    context = {"pageTitle": "FriendZone"}
     return render (request, 'index.html', context)
