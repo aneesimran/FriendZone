5643c2c750d1519295a4d4cbc83f2d70b3db0f13
Tahmeed Ahmed <tahmeed1998@hotmail.co.uk>
2018-11-30 14:18:40 +0000

    done register page

--- FriendZone/FriendZoneApp/urls.py
+++ FriendZone/FriendZoneApp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('', views.register, name = "register"),
+    path('register/', views.regisster),
+    path('index/', views.index)
     
 ]
--- FriendZone/FriendZoneApp/views.py
+++ FriendZone/FriendZoneApp/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
 def index(request):
-    context = {"pageTitle": "FriendZone"}
-    return render (request, 'index.html', context)
+    return render (request, 'index.html', {'content': 'ghana'})
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log user in !
+            return redirect('/index/')
+    else: 
+        form = UserCreationForm()
+    return render (request, 'register.html', {'form': form})
