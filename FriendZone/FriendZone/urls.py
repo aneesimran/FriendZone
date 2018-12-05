5326798e58c05b14742c91bacb4423863829495d
Anees Imran <aneesimran@outlook.com>
2018-12-05 16:04:39 +0000

    fixed the f up

--- FriendZone/FriendZone/urls.py
+++ FriendZone/FriendZone/urls.py
@@ -7,4 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FriendZoneApp.urls'))
-] + static(settings.MEDIA.URL, document_root = settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
